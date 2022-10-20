e69301d8b599cc1042d2be5b8d31451de8571d3a
Veronica-Alfr <veagalves@gmail.com>
2022-10-20 17:24:55 -0300

    fix: arrumado identação

--- src/jobs.py
+++ src/jobs.py
@@ -1,8 +1,9 @@
 from functools import lru_cache
 from csv import DictReader
 
+
 @lru_cache
 def read(path):
-   with open(path) as file:
-    data = DictReader(file, delimiter=",")
+    with open(path) as file:
+        data = DictReader(file, delimiter=",")
     return list(data)
