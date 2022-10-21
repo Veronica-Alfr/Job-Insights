ca2bd7bfa2855a3496bffd62af117ac84f9a31bb
Veronica-Alfr <veagalves@gmail.com>
2022-10-21 09:28:11 -0300

    feat: criado função que retorna se o salário está na faixa salarial

--- src/insights.py
+++ src/insights.py
@@ -61,28 +61,16 @@ def get_min_salary(path):
 
 
 def matches_salary_range(job, salary):
-    """Checks if a given salary is in the salary range of a given job
+    if not type(salary) is int:
+        raise ValueError("Only integers salaries are allowed")
+    if "min_salary" not in job or "max_salary" not in job:
+        raise ValueError("min_salary or max_salary is not in job")
+    if type(job["min_salary"]) != int or type(job["max_salary"]) != int:
+        raise ValueError("min_salary or max_salary is not integer")
+    if job["min_salary"] > job["max_salary"]:
+        raise ValueError("min_salary can't be greather than max_salary")
+    return job["min_salary"] <= salary <= job["max_salary"]
 
-    Parameters
-    ----------
-    job : dict
-        The job with `min_salary` and `max_salary` keys
-    salary : int
-        The salary to check if matches with salary range of the job
-
-    Returns
-    -------
-    bool
-        True if the salary is in the salary range of the job, False otherwise
-
-    Raises
-    ------
-    ValueError
-        If `job["min_salary"]` or `job["max_salary"]` doesn't exists
-        If `job["min_salary"]` or `job["max_salary"]` aren't valid integers
-        If `job["min_salary"]` is greather than `job["max_salary"]`
-        If `salary` isn't a valid integer
-    """
     pass
 
 
