c3fc16282e03c6c3fdab05f84579bd2df7c32e21
Veronica-Alfr <veagalves@gmail.com>
2022-10-21 18:10:36 -0300

    fix: arrumado identação de espaço

--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -5,4 +5,4 @@ def test_counter():
     # 16.., 1..
     path = "src/jobs.csv"
     assert count_ocurrences(path, "Javascript") == 32
-    assert  count_ocurrences(path, "Python") == 1570
+    assert count_ocurrences(path, "Python") == 1570
