ec08c75012d010533d2cc8b074ebd36ab324854e
Veronica-Alfr <veagalves@gmail.com>
2022-10-23 19:59:41 -0300

    fix: arrumado a quantidade de vezes que são retornados as palavras Python e Javascript

--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -2,7 +2,6 @@ from src.counter import count_ocurrences
 
 
 def test_counter():
-    # 16.., 1..
     path = "src/jobs.csv"
-    assert count_ocurrences(path, "Javascript") == 32
-    assert count_ocurrences(path, "Python") == 1570
+    assert count_ocurrences(path, "Javascript") == 122
+    assert count_ocurrences(path, "Python") == 1639
